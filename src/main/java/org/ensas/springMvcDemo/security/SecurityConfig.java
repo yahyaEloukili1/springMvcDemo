f95ac8b0f57e1128fcecf8e9795d87e9d842712f
yahya <yahya.ensas@hotmail.com>
2021-04-17 12:25:25 +0000

    inMemory authentication

---
+++ src/main/java/org/ensas/springMvcDemo/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.ensas.springMvcDemo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user1").password("{noop}1234").roles("USER");
+		auth.inMemoryAuthentication().withUser("user2").password("1234").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("1234").roles("ADMIN");
+		
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin();
+		http.authorizeRequests().anyRequest().authenticated();
+	}
+}
